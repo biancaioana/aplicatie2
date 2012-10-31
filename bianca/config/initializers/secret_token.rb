79db30d31efd94b48bbe30ffe2068ed34238b51b
Bianca <bianca@bianca-Lenovo-G550.(none)>
2012-10-31 11:45:56 +0200

    Bla...adaugare aplicatie

---
+++ bianca/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bianca::Application.config.secret_token = 'fd60513f3c166452ad36ad404c9f9fdfd6fe0ef47d90a4a13551ed8d1222737f906bae5901a6d6bc8d52d7b245db0323ef3f8d34bdd9ecb53e5128160ce08490'
